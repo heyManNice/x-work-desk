bd11aecba5ea1250b2b2d6b3cc1f305f6bb8584b
heyManNice <heyManNice@users.noreply.github.com>
2026-09-07 15:37:51 +0800

    style(clip): 格式化 hexval 判断为多行

--- src/clip.c
+++ src/clip.c
@@ -238,9 +238,12 @@ void clip_check(runtime *rt)
 
 static int hexval(char c)
 {
-    if (c >= '0' && c <= '9') return c - '0';
-    if (c >= 'a' && c <= 'f') return c - 'a' + 10;
-    if (c >= 'A' && c <= 'F') return c - 'A' + 10;
+    if (c >= '0' && c <= '9')
+        return c - '0';
+    if (c >= 'a' && c <= 'f')
+        return c - 'a' + 10;
+    if (c >= 'A' && c <= 'F')
+        return c - 'A' + 10;
     return -1;
 }
 
